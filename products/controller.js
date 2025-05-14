61327920f4ce6d3c94e511e310b74d82ec8744da
Nester <nesterkabatuku389@gmail.com>
2025-05-14 20:18:06 +0300

    updated the api doc

--- products/controller.js
+++ products/controller.js
@@ -10,7 +10,24 @@ const Product = require("../products/model");
  *   description: Product management
  */
 
-
+/**
+ * @swagger
+ * /products:
+ *   post:
+ *     summary: Create a new product
+ *     tags: [Products]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/Product'
+ *     responses:
+ *       201:
+ *         description: product created successfully
+ *       400:
+ *         description: Bad request
+ */
 // Create Product
 router.post("/", async (req, res) => {
   try {
@@ -21,6 +38,18 @@ router.post("/", async (req, res) => {
   }
 });
 
+/**
+ * @swagger
+ * /products:
+ *   get:
+ *     summary: Get all products
+ *     tags: [Products]
+ *     responses:
+ *       200:
+ *         description: List of products
+ *       500:
+ *         description: Internal server error
+ */
 // Get All Products
 router.get("/", async (req, res) => {
   try {
@@ -31,6 +60,26 @@ router.get("/", async (req, res) => {
   }
 });
 
+/**
+ * @swagger
+ * /products/{id}:
+ *   get:
+ *     summary: Get product by ID
+ *     tags: [Products]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: integer
+ *     responses:
+ *       500:
+ *         description: Internal server error
+ *       200:
+ *        description: product found
+ *       404:
+ *        description: product not found
+ */
 // Get Product by ID
 router.get("/:id", async (req, res) => {
   try {
@@ -45,6 +94,33 @@ router.get("/:id", async (req, res) => {
   }
 });
 
+/**
+ * @swagger
+ * /products/{id}:
+ *   put:
+ *     summary: Update a product
+ *     tags: [Products]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: integer
+ *         description: ID of the product to update
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/Product'
+ *     responses:
+ *       200:
+ *         description: Product updated successfully
+ *       400:
+ *         description: Bad request
+ *       404:
+ *         description: Product not found
+ */
 // Update Product
 router.put("/:id", async (req, res) => {
   try {
@@ -60,6 +136,25 @@ router.put("/:id", async (req, res) => {
   }
 });
 
+/**
+ * @swagger
+ * /products/{id}:
+ *   delete:
+ *     summary: Delete a product
+ *     tags: [Products]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: integer
+ *         description: ID of the product to delete
+ *     responses:
+ *       204:
+ *         description: product deleted
+ *       404:
+ *         description: product not found
+ */
 // Delete Product
 router.delete("/:id", async (req, res) => {
   try {
